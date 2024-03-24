4a70f3dcbe30e15fa99c751a40959d01a9048222
Brian K. <brianrotich044@gmail.com>
2024-03-24 12:28:03 +0300

    set up templates

--- index/models.py
+++ index/models.py
@@ -1,3 +1,15 @@
+from django.urls import reverse
 from django.db import models
 
 # Create your models here.
+class Course (models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("_detail", kwargs={"pk": self.pk})
+
--- index/views.py
+++ index/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Course
 
 # Create your views here.
 def index(request):
     return render(request,"index/index.html")
+
+class BlogListView(ListView):
+    model = Course
+    template_name = "index/index.html"
